afc6caa95c6333a69f4fa95280868901ff45b79d
Kerry Zheng <kerryzheng1215@gmail.com>
2024-03-16 19:53:19 -0400

    Add a php folder

--- Database/CreateTables.sql
+++ Database/CreateTables.sql
@@ -1,36 +1,36 @@
-CREATE TABLE [users] (
-  [id] integer PRIMARY KEY IDENTITY,
-  [username] varchar(255),
-  [password] varchar(255),
-  [created_at] datetime DEFAULT GETDATE()
+CREATE TABLE `users` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `username` varchar(255),
+  `password` varchar(255),
+  `created_at` datetime DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE [info] (
-  [user_id] integer PRIMARY KEY,
-  [DOB] date,
-  [Address] varchar(255),
-  [Phone_number] varchar(255),
-  [created_at] datetime DEFAULT GETDATE(),
-  FOREIGN KEY ([user_id]) REFERENCES [users] ([id])
+CREATE TABLE `info` (
+  `user_id` int PRIMARY KEY,
+  `DOB` date,
+  `Address` varchar(255),
+  `Phone_number` varchar(255),
+  `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
+  FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
 );
 
-CREATE TABLE [flag_lookup] (
-  [id] integer PRIMARY KEY,
-  [value] varchar(255)
+CREATE TABLE `flag_lookup` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `value` varchar(255)
 );
 
-CREATE TABLE [type_lookup] (
-  [id] integer PRIMARY KEY,
-  [value] varchar(255)
+CREATE TABLE `type_lookup` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `value` varchar(255)
 );
 
-CREATE TABLE [data] (
-  [user_id] integer,
-  [flag_id] integer,
-  [type_id] integer,
-  [created_at] datetime DEFAULT GETDATE(),
-  [amount] integer,
-  FOREIGN KEY ([user_id]) REFERENCES [users] ([id]),
-  FOREIGN KEY ([flag_id]) REFERENCES [flag_lookup] ([id]),
-  FOREIGN KEY ([type_id]) REFERENCES [type_lookup] ([id])
+CREATE TABLE `data` (
+  `user_id` int,
+  `flag_id` int,
+  `type_id` int,
+  `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
+  `amount` int,
+  FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
+  FOREIGN KEY (`flag_id`) REFERENCES `flag_lookup` (`id`),
+  FOREIGN KEY (`type_id`) REFERENCES `type_lookup` (`id`)
 );
