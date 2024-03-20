6d4e7798370e021548849a7cd7968786fd008ed5
Kerry Zheng <kerryzheng1215@gmail.com>
2024-03-20 19:38:25 -0400

    Added the home page

--- app/src/main/java/com/example/coinnest/HomeFragment.java
+++ app/src/main/java/com/example/coinnest/HomeFragment.java
@@ -3,14 +3,45 @@ package com.example.coinnest;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 
 public class HomeFragment extends Fragment {
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        ListView listView = view.findViewById(R.id.listBalances);
+
+        TextView currentDateTextView = view.findViewById(R.id.currentDateTextView);
+        String currentDate = new SimpleDateFormat("EEE, d MMM yyyy", Locale.getDefault()).format(new Date());
+        currentDateTextView.setText(currentDate);
+
+        // Apply the adapter to the ListView
+        String[] items = new String[] {"Credit Card 1 = $234.22", "Credit Card 2: $567.23", "Mortgage: $80,500.00",
+                "Car Loan: $35,000.00", "Student Loans: $20,000.00"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_list_item_1, items);
+        listView.setAdapter(adapter);
+
+        ListView billsListView = view.findViewById(R.id.upcomingBillsListView);
+        String[] bills = new String[] {"Credit Card: $30.00", "Credit Card 2: $50.00",
+                "Mortgage: $1,500.00" , "Car Loan: $550.00", "Student Loans: $1,000"};
+        ArrayAdapter<String> billsAdapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_list_item_1, bills);
+        billsListView.setAdapter(billsAdapter);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -22,55 +53,3 @@ public class HomeFragment extends Fragment {
 
 
 
-
-/*
-package com.example.coinnest;
-
-import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
-import android.content.Intent;
-import android.view.View;
-import com.google.android.material.card.MaterialCardView;
-
-public class HomeFragment extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        MaterialCardView balanceCard = findViewById(R.id.balanceCard);
-        balanceCard.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, OverviewActivity.class);
-                startActivity(intent);
-            }
-
-        });
-
-        MaterialCardView calendarCard = findViewById(R.id.calendarCard);
-        calendarCard.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, OverviewActivity.class);
-                startActivity(intent);
-            }
-
-        });
-
-        MaterialCardView upcomingBillsCard = findViewById(R.id.upcomingBillsCard);
-        upcomingBillsCard.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, OverviewActivity.class);
-                startActivity(intent);
-            }
-
-        });
-
-
-
-    }
-}
-*/
