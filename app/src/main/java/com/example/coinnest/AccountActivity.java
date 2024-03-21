f29c5e1bf38e582c75e752bda0a08bf208d4c880
Kerry Zheng <kerryzheng1215@gmail.com>
2024-03-21 17:16:51 -0400

    added the misc page

--- app/src/main/java/com/example/coinnest/AccountActivity.java
+++ app/src/main/java/com/example/coinnest/AccountActivity.java
@@ -1,6 +1,9 @@
 package com.example.coinnest;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class AccountActivity extends AppCompatActivity {
@@ -9,5 +12,12 @@ public class AccountActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account); // Set to your layout file
+        ListView listView = findViewById(R.id.myListView2); // Use findViewById directly on the Activity
+        String[] names = {"Change Password", "Change Email", "Change Name",};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, names); // Use 'this' instead of 'getContext()' if you're inside an Activity
+        listView.setAdapter(adapter);
     }
+
+
 }
+
