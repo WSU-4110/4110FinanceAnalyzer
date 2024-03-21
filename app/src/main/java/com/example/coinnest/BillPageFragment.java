8756002c967b1d424b6aa6cc0bad69d507f579ba
Kerry Zheng <kerryzheng1215@gmail.com>
2024-03-21 12:27:51 -0400

    Change the add bill button to white

--- app/src/main/java/com/example/coinnest/BillPageFragment.java
+++ app/src/main/java/com/example/coinnest/BillPageFragment.java
@@ -1,6 +1,7 @@
 package com.example.coinnest;
 
 import android.app.AlertDialog;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
